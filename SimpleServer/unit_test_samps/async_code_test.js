52cc751f381d7ca35cf7798fedfe06945bdbf319
guxu <745153679@qq.com>
2013-09-12 21:18:56 +0800

    create some unit test samples

---
+++ SimpleServer/unit_test_samps/async_code_test.js
@@ -0,0 +1,15 @@
+/**
+ * @author TonyGuu
+ */
+
+describe('User', function(){
+  describe('#save()', function(){
+    it('should save without error', function(done){
+      var user = new User('Luna');
+      user.save(function(err){
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+});
---
+++ SimpleServer/unit_test_samps/sync_code_test.js
@@ -0,0 +1,24 @@
+/**
+ * @author TonyGuu
+ */
+var assert = require("assert");
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+	
+	//test case
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(1));
+    });
+  });
+  
+  describe('#', function(){
+		
+		//test case
+	  it('', function(){
+	      
+	    });
+	  });
+  
+});
