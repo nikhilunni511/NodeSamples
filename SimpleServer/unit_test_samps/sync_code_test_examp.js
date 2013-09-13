ff576278321bf7756969961a2a1116b27167b882
guxu <745153679@qq.com>
2013-09-13 13:12:51 +0800

    。。

---
+++ SimpleServer/unit_test_samps/sync_code_test_examp.js
@@ -0,0 +1,25 @@
+/**
+ * @author TonyGuu
+ */
+var assert = require("assert");
+var should = require("should");
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+	
+	//test case
+    it('should return -1 when the value is not present', function(){
+      should.equal(-1, [1,2,3].indexOf(5));
+      should.equal(0, [1,2,3].indexOf(1));
+    });
+  });
+  
+  describe('#', function(){
+		
+		//test case
+	  it('', function(){
+	      
+	    });
+	  });
+  
+});
